4d4c87ed4f417dda2a5cb3aac13ef2395c0a5540
Shadowgun <shadowgun@i.ua>
2017-05-26 16:09:39 +0300

    provide spring 5 web client usage example

--- src/main/java/com/example/service/gitter/GitterConfiguration.java
+++ src/main/java/com/example/service/gitter/GitterConfiguration.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
@@ -18,10 +20,26 @@ import reactor.ipc.netty.http.client.HttpClient;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLEngine;
 import java.io.IOException;
+import java.time.Duration;
 
 @Configuration
 @EnableConfigurationProperties(GitterProperties.class)
 public class GitterConfiguration {
+
+    @Bean
+    @Primary
+    @Qualifier("Spring5GitterClient")
+    public GitterClient spring5GitterClient(GitterProperties gitterProperties) {
+        return query -> WebClient.create()
+                .get()
+                .uri(GitterUriBuilder.from(gitterProperties).queryParams(query).build().toUri())
+                .header("Authorization", "Bearer " + gitterProperties.getAuth().getToken())
+                .exchange()
+                .log()
+                .retryWhen(fe -> Flux.range(0, 10).delaySubscription(Duration.ofMillis(100)))
+                .checkpoint("received")
+                .flatMapMany(r -> r.bodyToFlux(MessageResponse.class));
+    }
 
     @Bean
     @Qualifier("ReactorGitterClient")
