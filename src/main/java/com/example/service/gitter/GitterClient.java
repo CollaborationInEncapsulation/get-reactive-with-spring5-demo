447897c5f98254ba48916929eadf70d669817f7f
Oleh Dokuka <shadowgun@i.ua>
2017-08-17 11:54:38 +0300

    migrate gitter client interface to publisher

--- src/main/java/com/example/service/gitter/GitterClient.java
+++ src/main/java/com/example/service/gitter/GitterClient.java
@@ -1,12 +1,13 @@
 package com.example.service.gitter;
 
 import com.example.service.gitter.dto.MessageResponse;
+import org.reactivestreams.Publisher;
 import org.springframework.util.MultiValueMap;
-
-import java.util.List;
 
 
 public interface GitterClient {
 
-    List<MessageResponse> getMessages(MultiValueMap<String, String> query);
+    Publisher<MessageResponse> getMessagesStream(MultiValueMap<String, String> query);
+
+    Publisher<MessageResponse> getLatestMessages();
 }
