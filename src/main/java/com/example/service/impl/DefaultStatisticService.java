52ae2177687b5069e025a6ef31ceec8daaeda5d1
Oleh Dokuka <shadowgun@i.ua>
2017-08-17 12:38:31 +0300

    update statistic service implementation
    we assume that spring framework somehow notify us that db changes and
    based on that event we may react and calculate new users statistic and
    then - send new value through the flow

--- src/main/java/com/example/service/impl/DefaultStatisticService.java
+++ src/main/java/com/example/service/impl/DefaultStatisticService.java
@@ -6,39 +6,55 @@ import com.example.repository.UserRepository;
 import com.example.service.StatisticService;
 import com.example.service.impl.utils.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+import reactor.core.publisher.Mono;
 
-import javax.transaction.Transactional;
+import java.time.Duration;
 
 @Service
 public class DefaultStatisticService implements StatisticService {
     private static final UserVM EMPTY_USER = new UserVM("", "");
 
     private final UserRepository userRepository;
+    private final Flux<UsersStatisticVM> statisticPublisher;
 
     @Autowired
-    public DefaultStatisticService(UserRepository userRepository) {
+    public DefaultStatisticService(ConfigurableApplicationContext context, UserRepository userRepository) {
         this.userRepository = userRepository;
+        this.statisticPublisher = Flux
+                .create(
+                        s -> context.addApplicationListener((MessageSavedEvent e) -> s.next(e.getSource())),
+                        FluxSink.OverflowStrategy.LATEST
+                )
+                .flatMap(s -> doGetUserStatistic())
+                .mergeWith(Mono.defer(this::doGetUserStatistic))
+                .cache(1);
     }
 
     @Override
-    @Transactional
-    public UsersStatisticVM getUsersStatistic() {
-        UserVM topActiveUser = userRepository.findAllOrderedByActivityDesc(new PageRequest(0, 1))
-                .map(UserMapper::toViewModelUnits)
-                .getContent()
-                .stream()
-                .findFirst()
-                .orElse(EMPTY_USER);
+    public Flux<UsersStatisticVM> usersStatisticStream() {
+        return statisticPublisher;
+    }
 
-        UserVM topMentionedUser = userRepository.findAllOrderedByMentionDesc(new PageRequest(0, 1))
-                .map(UserMapper::toViewModelUnits)
-                .getContent()
-                .stream()
-                .findFirst()
-                .orElse(EMPTY_USER);
+    private Mono<UsersStatisticVM> doGetUserStatistic() {
+        Mono<UserVM> topActiveUser = userRepository.findMostActive().map(UserMapper::toViewModelUnits);
 
-        return new UsersStatisticVM(topActiveUser, topMentionedUser);
+        Mono<UserVM> topMentionedUser = userRepository.findMostPopular().map(UserMapper::toViewModelUnits);
+
+        return Mono.zip(
+                ar -> new UsersStatisticVM((UserVM) ar[0], (UserVM) ar[1]),
+                topActiveUser.transform(this::addResilience),
+                topMentionedUser.transform(this::addResilience)
+        );
+    }
+
+    private Mono<UserVM> addResilience(Mono<UserVM> input) {
+        return input
+                .timeout(Duration.ofSeconds(2))
+                .defaultIfEmpty(EMPTY_USER)
+                .onErrorReturn(EMPTY_USER);
     }
 }
