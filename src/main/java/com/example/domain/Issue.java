fd1debcbd9b3292302a51a378d788248b8e4a4b0
Oleh Dokuka <shadowgun@i.ua>
2017-08-17 11:46:58 +0300

    migrate domain entities to mongo documents

--- src/main/java/com/example/domain/Issue.java
+++ src/main/java/com/example/domain/Issue.java
@@ -1,24 +1,18 @@
 package com.example.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.NonNull;
-import lombok.experimental.Accessors;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import org.springframework.data.annotation.PersistenceConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.persistence.Embeddable;
-import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
-@Accessors(chain = true)
-@NoArgsConstructor
-@AllArgsConstructor(staticName = "of")
-@EqualsAndHashCode
-@Embeddable
+@Value
+@Document(collection = "issues")
+@RequiredArgsConstructor(staticName = "of", onConstructor = @__(@PersistenceConstructor))
 public class Issue implements Serializable {
+
     @NonNull
-    @NotNull
-    @Getter
-    private Long id;
+    private final Long id;
 }
--- src/main/java/com/example/domain/Mention.java
+++ src/main/java/com/example/domain/Mention.java
@@ -1,62 +1,21 @@
 package com.example.domain;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import lombok.experimental.Accessors;
-import org.hibernate.annotations.NotFound;
-import org.hibernate.annotations.NotFoundAction;
+import lombok.Value;
+import org.springframework.data.annotation.PersistenceConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.persistence.Column;
-import javax.persistence.ConstraintMode;
-import javax.persistence.Embeddable;
-import javax.persistence.EmbeddedId;
-import javax.persistence.Entity;
-import javax.persistence.ForeignKey;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
-@Data
-@Entity
-@Table(name = "mention")
-@Accessors(chain = true)
-@RequiredArgsConstructor(staticName = "of")
-@NoArgsConstructor
+@Value
+@Document(collection = "mentions")
+@RequiredArgsConstructor(staticName = "of", onConstructor = @__(@PersistenceConstructor))
 public class Mention implements Serializable {
-    @NotNull
+
     @NonNull
-    @EmbeddedId
-    private Key id;
+    private final String userId;
 
-    @Getter
-    @ManyToOne
-    @NotFound(action = NotFoundAction.IGNORE)
-    @JoinColumn(name = "user_id",
-            referencedColumnName = "id",
-            insertable = false,
-            updatable = false,
-            foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
-    private User user = null;
-
-    @Embeddable
-    @Data
-    @Accessors(chain = true)
-    @RequiredArgsConstructor(staticName = "of")
-    @NoArgsConstructor
-    public static class Key implements Serializable {
-        @NotNull
-        @NonNull
-        @Column(name = "message_id")
-        private String messageId;
-
-        @NotNull
-        @NonNull
-        @Column(name = "user_id")
-        private String userId;
-    }
+    @NonNull
+    private final String screenName;
 }
