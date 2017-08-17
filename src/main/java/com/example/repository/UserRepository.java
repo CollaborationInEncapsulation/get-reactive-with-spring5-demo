e773ce95451ceb3582c71f5a7d8283598a92d1d9
Oleh Dokuka <shadowgun@i.ua>
2017-08-17 11:44:39 +0300

    migrate spring data reactive repository

--- src/main/java/com/example/repository/UserRepository.java
+++ src/main/java/com/example/repository/UserRepository.java
@@ -1,25 +1,12 @@
 package com.example.repository;
 
 import com.example.domain.User;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
-@Repository
-public interface UserRepository extends JpaRepository<User, String> {
-    @Query("SELECT user " +
-            "FROM Message messages " +
-            "INNER JOIN messages.user user " +
-            "GROUP BY user " +
-            "ORDER BY COUNT(messages) DESC")
-    Page<User> findAllOrderedByActivityDesc(Pageable pageable);
 
-    @Query("SELECT user " +
-            "FROM Mention mentions " +
-            "INNER JOIN mentions.user user " +
-            "GROUP BY user " +
-            "ORDER BY COUNT(mentions) DESC")
-    Page<User> findAllOrderedByMentionDesc(Pageable pageable);
+public interface UserRepository {
+
+    Mono<User> findMostActive();
+
+    Mono<User> findMostPopular();
 }
