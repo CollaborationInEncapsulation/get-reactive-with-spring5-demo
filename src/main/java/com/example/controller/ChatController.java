1f8a4710ed6759effaa440ef259132b7db5641b6
Oleh Dokuka <shadowgun@i.ua>
2017-08-17 12:51:13 +0300

    move to reactive web handlers

--- src/main/java/com/example/controller/ChatController.java
+++ src/main/java/com/example/controller/ChatController.java
@@ -1,42 +1,15 @@
 package com.example.controller;
 
-import com.example.controller.vm.MessageVM;
-import com.example.controller.vm.UsersStatisticVM;
-import com.example.service.MessageService;
-import com.example.service.StatisticService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 @Controller
 @RequestMapping
 public class ChatController {
 
-    private final MessageService messageService;
-    private final StatisticService statisticService;
-
-    @Autowired
-    public ChatController(MessageService messageService, StatisticService statisticService) {
-        this.messageService = messageService;
-        this.statisticService = statisticService;
-    }
-
     @GetMapping
-    public String index(Model model) {
-        List<MessageVM> messages = messageService.latest();
-        UsersStatisticVM statistic = statisticService.getUsersStatistic();
-
-        model.addAttribute("messages", messages);
-        model.addAttribute("statistic", statistic);
-
-        if (messages != null && messages.size() > 0) {
-            model.addAttribute("cursor", messages.get(messages.size() - 1).getId());
-        }
-
+    public String index() {
         return "chat";
     }
 }
