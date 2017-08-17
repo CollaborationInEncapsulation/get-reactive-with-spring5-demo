81044826579e2692bd0e8d3ced7f7fc29f5218da
Oleh Dokuka <shadowgun@i.ua>
2017-08-17 12:32:14 +0300

    integrate mappers with reactive flow

--- src/main/java/com/example/service/impl/utils/MessageMapper.java
+++ src/main/java/com/example/service/impl/utils/MessageMapper.java
@@ -7,42 +7,30 @@ import com.example.domain.Message;
 import com.example.domain.User;
 import com.example.service.gitter.dto.MessageResponse;
 import com.example.service.gitter.dto.Url;
+import reactor.core.publisher.Flux;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.stream.Collectors;
 
 public final class MessageMapper {
     private MessageMapper() {
     }
 
-    public static List<MessageVM> toViewModelUnits(Iterable<MessageResponse> messages) {
+    public static Flux<MessageVM> toViewModelUnits(Flux<MessageResponse> messages) {
         if (messages == null) {
             return null;
         }
 
-        List<MessageVM> vms = new ArrayList<>();
 
-        for (MessageResponse message : messages) {
-            vms.add(toViewModelUnit(message));
-        }
-
-        return vms;
+        return messages.map(MessageMapper::toViewModelUnit);
 
     }
 
-    public static List<Message> toDomainUnits(Iterable<MessageResponse> messages) {
+    public static Flux<Message> toDomainUnits(Flux<MessageResponse> messages) {
         if (messages == null) {
             return null;
         }
 
-        List<Message> persistable = new ArrayList<>();
-
-        for (MessageResponse message : messages) {
-            persistable.add(toDomainUnit(message));
-        }
-
-        return persistable;
+        return messages.map(MessageMapper::toDomainUnit);
     }
 
     private static Message toDomainUnit(MessageResponse message) {
@@ -57,10 +45,14 @@ public final class MessageMapper {
                 .sent(message.getSent())
                 .readBy(message.getReadBy())
                 .unread(message.getUnRead())
-                .user(User.of(message.getFromUser().getId(), message.getFromUser().getUsername()))
+                .user(User.of(
+                        message.getFromUser().getId(),
+                        message.getFromUser().getUsername(),
+                        message.getFromUser().getDisplayName()
+                ))
                 .urls(message.getUrls().stream().map(Url::getUrl).toArray(String[]::new))
                 .mentions(message.getMentions().stream().filter(m -> m.getUserId() != null)
-                        .map(m -> Mention.of(Mention.Key.of(message.getId(), m.getUserId())))
+                        .map(m -> Mention.of(m.getUserId(), m.getScreenName()))
                         .collect(Collectors.toSet()))
                 .issues(message.getIssues().stream()
                         .map(i -> Issue.of(Long.valueOf(i.getNumber()))).collect(Collectors.toSet()))
