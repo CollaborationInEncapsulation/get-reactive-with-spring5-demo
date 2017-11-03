f704639f9f70d5c61ec8db2dbd84ee27d181b52b
Oleh Dokuka <shadowgun@i.ua>
2017-11-03 22:53:26 +0300

    Refactor to Reactive. Step 3.2: refactor Message service

--- src/main/java/com/example/service/impl/utils/MessageMapper.java
+++ src/main/java/com/example/service/impl/utils/MessageMapper.java
@@ -7,42 +7,30 @@ import com.example.domain.Message;
 import com.example.domain.User;
 import com.example.service.gitter.dto.MessageResponse;
 import com.example.service.gitter.dto.Url;
+import reactor.core.publisher.Flux;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.stream.Collectors;
 
 public final class MessageMapper {
     private MessageMapper() {
     }
 
-    public static List<MessageVM> toViewModelUnits(Iterable<MessageResponse> messages) {
+    public static Flux<MessageVM> toViewModelUnits(Flux<MessageResponse> messages) {
         if (messages == null) {
             return null;
         }
 
-        List<MessageVM> vms = new ArrayList<>();
 
-        for (MessageResponse message : messages) {
-            vms.add(toViewModelUnit(message));
-        }
-
-        return vms;
+        return messages.map(MessageMapper::toViewModelUnit);
 
     }
 
-    public static List<Message> toDomainUnits(Iterable<MessageResponse> messages) {
+    public static Flux<Message> toDomainUnits(Flux<MessageResponse> messages) {
         if (messages == null) {
             return null;
         }
 
-        List<Message> persistable = new ArrayList<>();
-
-        for (MessageResponse message : messages) {
-            persistable.add(toDomainUnit(message));
-        }
-
-        return persistable;
+        return messages.map(MessageMapper::toDomainUnit);
     }
 
     private static Message toDomainUnit(MessageResponse message) {
